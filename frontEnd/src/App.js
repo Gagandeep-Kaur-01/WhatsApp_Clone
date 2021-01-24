c6a75af6207dae9f885b89dd814c5f06fc9ddb2b
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 12:57:56 +0530

    removed unusual heading

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     // BEM naming conventions 
-    <div className="app">
-      <h1> Hey! Another project</h1>   
+    <div className="app">  
 
       <div className="app__body">
         {/* Sidebar */}
         {/* Chat */}
       </div>   
+      
     </div>
   );
 }
