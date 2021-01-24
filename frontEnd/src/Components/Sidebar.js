98b33fc6458f6330f15ab38af9b3cda1956cd2b9
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 13:56:50 +0530

    added 3 icons

--- frontEnd/src/Components/Sidebar.js
+++ frontEnd/src/Components/Sidebar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Avatar } from "@material-ui/core"
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import './Sidebar.css'
 
@@ -9,6 +12,11 @@ function Sidebar() {
 
             <div className="sidebar__header">
                 <Avatar />
+                <div className="sidebar__headerRight">
+                    <DonutLargeIcon />
+                    <ChatIcon />
+                    <MoreVertIcon />
+                </div>
             </div>
 
             <div className="sidebar__search">
