d9541df139cf28be47a3ad49df4e926704ec4e8f
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 13:47:33 +0530

    inserted 'Avatar'

--- frontEnd/src/Components/Sidebar.js
+++ frontEnd/src/Components/Sidebar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Avatar } from "@material-ui/icons"
+
 import './Sidebar.css'
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sidebar__header">
 
+            <div className="sidebar__header">
+                <Avatar />
             </div>
 
             <div className="sidebar__search">
@@ -13,8 +16,9 @@ function Sidebar() {
             </div>
 
             <div className="sidebar_chats">
-                
+
             </div>
+
         </div>
     )
 }
