af272566b00c76135922880ba89b01c93a9e6bfe
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 12:27:35 +0530

    firebase configuration

---
+++ frontEnd/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDtMOZAxTd6XdGyYOilCaJt5CsKYnXvFEo",
+    authDomain: "whatsapp-clone-9d9eb.firebaseapp.com",
+    projectId: "whatsapp-clone-9d9eb",
+    storageBucket: "whatsapp-clone-9d9eb.appspot.com",
+    messagingSenderId: "550612015863",
+    appId: "1:550612015863:web:348ed78eef7b8bfe66d1f2"
+  };
