14dabe657c34200c207141fde2e592f93281d98c
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 19:24:06 +0530

    wrapped the icons in 'IconButton'

--- frontEnd/src/Components/Sidebar.js
+++ frontEnd/src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar } from "@material-ui/core"
+import { Avatar, IconButton } from "@material-ui/core"
 import DonutLargeIcon from "@material-ui/icons/DonutLarge"
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -13,9 +13,17 @@ function Sidebar() {
             <div className="sidebar__header">
                 <Avatar />
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
-                    <ChatIcon />
-                    <MoreVertIcon />
+                    <IconButton>
+                      <DonutLargeIcon />
+                    </IconButton>
+                    
+                    <IconButton>
+                      <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                      <MoreVertIcon />
+                    </IconButton>                    
                 </div>
             </div>
 
