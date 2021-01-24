a9f768cc4918798d83a4a75c096090d58a469d1d
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-24 12:42:02 +0530

    added div section & put the things in comments, which have to do next

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     // BEM naming conventions 
     <div className="app">
-      <h1> Hey! Another project</h1>      
+      <h1> Hey! Another project</h1>   
+
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>   
     </div>
   );
 }
