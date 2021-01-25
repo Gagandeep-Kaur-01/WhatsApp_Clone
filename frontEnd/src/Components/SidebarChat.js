55dabf448d73954bcbf882a0cd4b9876b1c72733
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-25 11:15:53 +0530

    check avatar using another random string

--- frontEnd/src/Components/SidebarChat.js
+++ frontEnd/src/Components/SidebarChat.js
@@ -5,7 +5,7 @@ import './SidebarChat.css'
 function SidebarChat() {
     return(
         <div className="sidebarChat">
-            <Avatar src="https://avatars.dicebear.com/api/human/ghff123.svg" />
+            <Avatar src="https://avatars.dicebear.com/api/human/hjnbfr123.svg" />
             <div className="sidebarChat__info">
                 <h2> Room Name </h2>
                 <p>Last message...</p> 
