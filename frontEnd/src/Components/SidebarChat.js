780e4b82b36237fa9c188d3447094a157a385cb6
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-25 11:18:58 +0530

    creating useEffect for taking random avatar(s)

--- frontEnd/src/Components/SidebarChat.js
+++ frontEnd/src/Components/SidebarChat.js
@@ -3,9 +3,15 @@ import { Avatar } from "@material-ui/core"
 import './SidebarChat.css'
 
 function SidebarChat() {
+
+    useEffect(() => {
+
+    }, [])
+
+
     return(
         <div className="sidebarChat">
-            <Avatar src="https://avatars.dicebear.com/api/human/hjnbfr123.svg" />
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2> Room Name </h2>
                 <p>Last message...</p> 
