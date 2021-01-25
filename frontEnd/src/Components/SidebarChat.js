bfdff16ea42461cee6fbe209a4e14db773ffb70c
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-25 11:03:33 +0530

    working with chat functionality, added 'Avatar'

--- frontEnd/src/Components/SidebarChat.js
+++ frontEnd/src/Components/SidebarChat.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Avatar } from "@material-ui/core"
 import './SidebarChat.css'
 
 function SidebarChat() {
     return(
         <div className="sidebarChat">
-            Sidebar Chat
+            <Avatar />
         </div>
     )
 }
