5dcf20b32e56a00858e3974d4b75b18979a0e8f1
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-25 11:12:02 +0530

    added src for taling 'Avatar'

--- frontEnd/src/Components/SidebarChat.js
+++ frontEnd/src/Components/SidebarChat.js
@@ -5,7 +5,7 @@ import './SidebarChat.css'
 function SidebarChat() {
     return(
         <div className="sidebarChat">
-            <Avatar />
+            <Avatar src="https://avatars.dicebear.com/api/human/ghff123.svg" />
             <div className="sidebarChat__info">
                 <h2> Room Name </h2>
                 <p>Last message...</p> 
