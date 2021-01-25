c379a62ca49c741a5c3c56799c4de7058b029804
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-25 16:37:30 +0530

    wporking with the footer of chat box

--- frontEnd/src/Components/Chat.js
+++ frontEnd/src/Components/Chat.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from "@material-ui/icons";
 
 import React, { useState, useEffect } from 'react';
 import "./Chat.css";
@@ -53,7 +53,14 @@ function Chat() {
             </div>
 
             <div className="chat__footer">
+                <InsertEmoticonIcon />
 
+                <form>
+                    <input type="text" />
+                    <button> Send a message </button>
+                </form>
+
+                <MicIcon />
             </div>
         </div> 
     )
