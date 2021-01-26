17f4ca134804824d52165f0bce99cbf5ccc8e821
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-26 11:04:50 +0530

    added firebase config

--- frontEnd/src/firebase.js
+++ frontEnd/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDtMOZAxTd6XdGyYOilCaJt5CsKYnXvFEo",
     authDomain: "whatsapp-clone-9d9eb.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "550612015863",
     appId: "1:550612015863:web:348ed78eef7b8bfe66d1f2"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
