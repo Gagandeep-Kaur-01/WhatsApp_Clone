30a07c97f997e0d6a44b6eacdbc7c8a2f716b4c4
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-27 18:26:39 +0530

    imported reducer & initialState

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducer, { initialState } from "./redux/reducer";
 
 ReactDOM.render(
   <React.StrictMode>
