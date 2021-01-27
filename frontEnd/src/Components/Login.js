1a217d66956f9e038f4c3635f75fdf0fb1fe91f8
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-27 18:34:18 +0530

    added dispatch for user

--- frontEnd/src/Components/Login.js
+++ frontEnd/src/Components/Login.js
@@ -3,6 +3,7 @@ import { Button } from "@material-ui/core";
 import "./Login.css";
 import { auth, provider } from '../firebase';
 import { useStateValue } from '../redux/StateProvider';
+import { actionTypes } from '../redux/reducer';
 
 function Login() {
     const [{}, dispatch] = useStateValue();
@@ -11,7 +12,12 @@ function Login() {
     const signIn = () => {
         auth
           .signInWithPopup(provider)
-          .then((result) => console.log(result))
+          .then((result) =>{
+              dispatch({
+                  type: actionTypes.SET_USER,
+                  user: result.user,
+              })
+          })
           .catch((error) => alert(error.message));
     };
 
