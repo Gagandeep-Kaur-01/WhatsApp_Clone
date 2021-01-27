99c62c634a5de22dea3cb0706977979a89b72955
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-27 18:27:44 +0530

    imported StateProvider

--- frontEnd/src/index.js
+++ frontEnd/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import reducer, { initialState } from "./redux/reducer";
+import { StateProvider } from "./redux/StateProvider";
 
 ReactDOM.render(
   <React.StrictMode>
