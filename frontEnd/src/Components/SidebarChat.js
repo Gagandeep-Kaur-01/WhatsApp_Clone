ebe8f953e7a9fce7f6b8b76dceecba6579f38d06
Gagandeep-Kaur-01 <gagandeep07kaur@gmail.com>
2021-01-27 13:51:35 +0530

    added link for room id, working with: on clicking room display messages of that room

--- frontEnd/src/Components/SidebarChat.js
+++ frontEnd/src/Components/SidebarChat.js
@@ -24,13 +24,15 @@ function SidebarChat({ id, name, addNewChat }) {
 
 
     return !addNewChat ? (
-        <div className="sidebarChat">
+        <Link to= {`/rooms/${id}`}>
+          <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
             <div className="sidebarChat__info">
                 <h2> {name} </h2>
                 <p>Last message...</p> 
             </div>
-        </div>
+          </div>
+        </Link> 
     ) : (
         <div onClick= {createChat} className="sidebarChat">
             <h2> Add new Chat</h2>
